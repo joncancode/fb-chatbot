8340577a11c1685868df918182fce1c895cab1a7
joncancode <jc.haines19@gmail.com>
2017-11-14 00:43:51 -0500

    added token plus the post route

--- index.js
+++ index.js
@@ -17,12 +17,49 @@ app.get('/', function(req, res) {
     res.send('chatbot\'s first words')
 })
 
+//need to add page token here
+let token = "page token"
+
+//for facebook
 app.get('/webhook/', function(req, res) {
     if (req.query['hub.verify_token'] === 'password123'){
         res.send(req.query['hub.challenge'])
     }
     res.send('incorrect token')
 })
+
+app.post('/webhook', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging
+        for (let i=0; i< messaging_events.length; i++){
+            let event = messaging_events[i]
+            let sender = event.sender.id
+            if (event.message && event.message.text){
+                let text = event.message.text
+                sendText(sender, "text echo: " + text.substring(0, 100))
+            }
+        }
+        res.sendStatus(200)
+})
+
+function sendText (sender, text){
+    let messageData = {text: text}
+    request({
+        //need to add fb page url here
+        url: "https://facebook.com/messages",
+        qs: {access_token: token},
+        method: "POST",
+        json: {
+            recipient: {id: sender},
+            message: messageData
+        }
+    }, function(error, response, body){
+        if (error) {
+            console.log('error sending')
+        } else if (response.body.error){
+            console.log('response body error')
+        }
+    })
+}
 
 app.listen(app.get('port'), function() {
     console.log(`running on port`, port)
